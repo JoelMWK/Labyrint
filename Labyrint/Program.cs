c0b3d90475ea08e04ba226405595fb8fa9c1ccfa
Joel Kustemo <kustemo.joel@elev.ga.ntig.se>
2021-12-01 12:48:06 +0100

    first commit

---
+++ Labyrint/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+string room = "mancave";
+string action;
+
+while (room != "exit")
+{
+    if (room == "mancave")
+    {
+        Console.WriteLine("You are in your mancave, if you head up you get to the store\n");
+
+        Console.WriteLine("Where do you want to go?");
+        action = Console.ReadLine();
+        Console.Clear();
+
+        if (action == "up")
+        {
+            room = "store";
+        }
+        else
+        {
+            Console.WriteLine("Stupid");
+        }
+    }
+
+    else if (room == "store")
+    {
+        Console.WriteLine("You are now in the store, if you go down you get back to your mancave");
+        Console.WriteLine("Or go right to the bathroom\n");
+
+        Console.WriteLine("Where do you want to go?");
+        action = Console.ReadLine();
+        Console.Clear();
+
+        if (action == "down")
+        {
+            room = "mancave";
+        }
+
+        else if (action == "right")
+        {
+            room = "bathroom";
+        }
+        else
+        {
+            Console.WriteLine("Stupid");
+        }
+
+        if (room == "bathroom")
+        {
+            Console.WriteLine("You are in the bathroom, if go left you head back to the store.");
+            Console.WriteLine("If you want to exit jump down the toilet\n");
+            action = Console.ReadLine();
+            Console.Clear();
+
+            if (action == "left")
+            {
+                room = "store";
+            }
+            else if (action == "jump" || action == "exit")
+            {
+                room = "exit";
+            }
+            else
+            {
+                Console.WriteLine("Stupid");
+            }
+        }
+    }
+}
